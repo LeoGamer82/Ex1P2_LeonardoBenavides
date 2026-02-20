e7ccfceae9c50738ca5da2cf16b14e29490d4f52
LeoB <leobenavides2007@gmail.com>
2026-02-20 13:28:20 -0600

    Laboratorio Inicializado

---
+++ src/ex1p2_leonardobenavides/Ex1P2_LeonardoBenavides.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex1p2_leonardobenavides;
+
+/**
+ *
+ * @author leobe
+ */
+public class Ex1P2_LeonardoBenavides {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
